b52ac8f1ffbeeb23b6934c643d9da669950d572a
Vincent <vincent@fullstack.pro>
2018-07-09 16:44:56 +0200

    Emojify only if message is a string

--- Logger/formatters/emojify.js
+++ Logger/formatters/emojify.js
@@ -2,10 +2,12 @@ const { format } = require("winston");
 const emoji = require("node-emoji");
 
 const emojify = format((info, opts) => {
-    if (opts) {
-        info.message = emoji.emojify(info.message);
-    } else {
-        info.message = emoji.strip(info.message);
+    if (typeof info.message == "string") {
+        if (opts) {
+            info.message = emoji.emojify(info.message);
+        } else {
+            info.message = emoji.strip(info.message);
+        }
     }
 
     return info;
